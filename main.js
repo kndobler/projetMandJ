6a6c40d761835f57af6d77f91aea25bf6f7f08da
Kevin DOBLER <kevin.dobler@outlook.fr>
2024-08-08 12:00:56 +0200

    modification élément graphique sur la partie du tableau des scores

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/*Debut de la partit JavaScript qui va nous permettre de créer la logique du jeu
+Dans cette partit nous allons pouvoir creer des éléments et des */
+
 let maVariable = 2;
 let div = document.querySelector('div');
 console.log(maVariable)
